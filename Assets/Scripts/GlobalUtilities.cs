cb9263d4d708e955a212373de501e14ca68aa55a
gaucht <gaucht@wit.edu>
2016-10-25 22:26:32 -0400

    Added arms for FPS and some basic Weapons.  Player can run and attack with animations.  Removed edits to the FPSCharacterController and added global static variables for input and animation names.

---
+++ Assets/Scripts/GlobalUtilities.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MedievalMayhem.Utilities {
+	public static class GlobalUtilities
+	{
+
+		/*
+		 * These strings are so if we change the animation properties
+		 * we don't need to change everywhere they are used. Except for the FirstPersonController.cs
+		 * 
+		 * Please use these references not static strings when using the animators
+		 */
+
+		// PlayerAnimator
+		public static string IDLE_NO_WEAPON = "IdleNoWeapon";
+		public static string IDLE_WITH_WEAPON = "IdleWithWeapon";
+		public static string RUN = "Run";
+		public static string WALK = "Walk";
+		public static string ATTACK_2_NO_WEAPON = "PunchLeft";
+		public static string ATTACK_1_NO_WEAPON = "PunchRight";
+		public static string ATTACK_2_WEAPON = "SwingLeftRight";
+		public static string ATTACK_1_WEAPON = "SwingUpDown";
+		public static string DEFAULT_ANIM = "Default";
+
+		//input
+		public static string ATTACK_1 = "Fire1";
+		public static string ATTACK_2 = "Fire2";
+		public static string INPUT_RUN = "Run";
+		public static string INPUT_LEFT_RIGHT = "Horizontal";
+		public static string INPUT_FORWARD_BACKWARD = "Vertical";
+
+
+	}
+}
+
---
+++ Assets/Scripts/PlayerFighting.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using MedievalMayhem.Utilities;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace MedievalMayhem.Player {
+	public class PlayerFighting : MonoBehaviour {
+
+		/**
+		 *  _ variables denote private variables
+		 */
+
+		[SerializeField] private Animator _playerAnimator;
+		[SerializeField] private GameObject primary;
+
+		private bool _hasWeapon;
+
+		public void Start() {
+
+			this._hasWeapon = (primary != null);
+
+			Debug.Log ("Has Weapon: " + this._hasWeapon);
+
+			if (this._hasWeapon) {
+				Debug.Log ("Has Weapon");
+				this._playerAnimator.Play (GlobalUtilities.IDLE_WITH_WEAPON);
+			} else {
+				Debug.Log ("Has No Weapon");
+				this._playerAnimator.Play (GlobalUtilities.IDLE_NO_WEAPON);
+			}
+		}
+
+		public void Update() {
+
+			// Check if we are trying to attack
+			bool attack1 = CrossPlatformInputManager.GetButtonDown(GlobalUtilities.ATTACK_1);
+			bool attack2 = CrossPlatformInputManager.GetButtonDown (GlobalUtilities.ATTACK_2);
+
+			if (attack1) {
+				if (this._hasWeapon) {
+					this._playerAnimator.SetTrigger (GlobalUtilities.ATTACK_1_WEAPON);
+				} else {
+					this._playerAnimator.SetTrigger (GlobalUtilities.ATTACK_1_NO_WEAPON);
+				}
+			} else if (attack2) {
+				if (this._hasWeapon) {
+					this._playerAnimator.SetTrigger (GlobalUtilities.ATTACK_2_WEAPON);
+				} else {
+					this._playerAnimator.SetTrigger (GlobalUtilities.ATTACK_2_NO_WEAPON);
+				}
+			}
+
+			// handle moving animations here
+			// Read input
+			float horizontal = CrossPlatformInputManager.GetAxis(GlobalUtilities.INPUT_LEFT_RIGHT);
+			float vertical = CrossPlatformInputManager.GetAxis(GlobalUtilities.INPUT_FORWARD_BACKWARD);
+
+			bool isMoving = (horizontal != 0 || vertical != 0);
+
+			bool isWalking = !CrossPlatformInputManager.GetButton(GlobalUtilities.INPUT_RUN);
+
+			this._playerAnimator.SetBool (GlobalUtilities.WALK, (isWalking && isMoving));
+			this._playerAnimator.SetBool (GlobalUtilities.RUN, (!isWalking && isMoving));
+			this._playerAnimator.SetBool (GlobalUtilities.IDLE_NO_WEAPON, (!isMoving && !this._hasWeapon));
+			this._playerAnimator.SetBool (GlobalUtilities.IDLE_WITH_WEAPON, (!isMoving && this._hasWeapon));
+
+			if (this._playerAnimator.GetBool (GlobalUtilities.RUN)) {
+				this._playerAnimator.Play (GlobalUtilities.RUN);
+			} else {
+				if (this._playerAnimator.GetBool (GlobalUtilities.IDLE_NO_WEAPON)) {
+					this._playerAnimator.CrossFade (GlobalUtilities.IDLE_NO_WEAPON, 0.25f);
+				} else {
+					this._playerAnimator.CrossFade (GlobalUtilities.IDLE_WITH_WEAPON, 0.25f);
+				}
+			}
+
+		}
+	}
+}
